99324872f6db77df7213e7b020682363f404f056
Javier Saez <javiersaezgallego@gmail.com>
2020-06-19 15:46:42 +0200

    Added sensor distribution plot

--- storpdm/visualize.py
+++ storpdm/visualize.py
@@ -10,6 +10,11 @@ def visualise_sensor_correlation_all_engine(data_df):
     -----------
         data_df : pd.dataFrame)
             Dataframe with engine data to be plotted.
+
+    Returns
+    --------
+    matplotlib.figure.Figure
+        Heatmap representing correlation between features
     """
 
     # Remove the time column from the dataframe.
@@ -29,3 +34,38 @@ def visualise_sensor_correlation_all_engine(data_df):
 
     return corr_fig
 
+
+def visualise_sensor_data_distribution(dataset_df):
+    """Plot and save the sensor data distributions for all engines.
+    
+    Parameters
+    -----------
+    data_df:  pd.dataframe
+        Dataframe with engine data to be plotted.
+
+    Returns
+    -------
+    matplotlib.figure.Figure
+        Figure with subplots, one distribution in each one.
+    """
+
+    # Prepare dataset for plot.
+    plotted_dataset_df = dataset_df.copy()
+
+    columns = plotted_dataset_df.columns
+
+    figs_per_row = 4
+    n_columns = len(columns)
+    n_rows = (int(n_columns / figs_per_row)) + 1
+
+    # Create plot.
+    fig, axes = plt.subplots(n_rows, figs_per_row,figsize=(15, 15))
+    axes = axes.flatten()
+    fig.suptitle('Sensor Distributions - All Engines')
+
+    for column, ax in zip(columns, axes):
+        ax = sns.distplot(plotted_dataset_df[column], ax = ax, label = column)
+        ax.legend(loc=1)
+
+    # Save plot.
+    return fig
